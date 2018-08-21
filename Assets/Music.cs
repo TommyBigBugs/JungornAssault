fe416d9392b851bdfa7dbd3250e023440e0cb0e1
TommyBigBugs <39831981+TommyBigBugs@users.noreply.github.com>
2018-08-21 09:57:57 +0100

    Adding camera and waypoints to game

---
+++ Assets/Music.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music : MonoBehaviour {
+
+    // Use this for initialization
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start () {
+        Invoke("LoadFirstScene", 2f);
+        
+
+    }
+
+    void LoadFirstScene()
+    {
+       SceneManager.LoadScene(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
